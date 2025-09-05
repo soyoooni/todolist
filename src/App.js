3fe464ac281d66f15c594c312c5a423ee30f4ce9
asdf <asdf@asdf.com>
2025-09-05 15:14:08 +0900

    버튼까지 만듬

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const TodoItemInputField = (props) => {
-  return(<div>hi</div>);
+  const [input, setInput] =useState("");
+  console.log(input);
+
+  return(<div>
+    <TextField
+    id="todo-item-input"
+    label="Todo Item"
+    variant="outlined"
+    onChange={(e) => setInput(e.target.value)}
+    value={input}
+    />
+    <Button variant='outlined'>Submit</Button>
+  </div>);
 }
 
 function App() {
