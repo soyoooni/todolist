28d8ab70652ecaf85e2a6545e2457e33af3cabe0
asdf <asdf@asdf.com>
2025-09-10 16:57:19 +0900

    m3-2

--- src/App.js
+++ src/App.js
@@ -157,6 +157,7 @@ function App() {
           todoItemContent: doc.data().todoItemContent,
           isFinished: doc.data().isFinished,
           createdTime: doc.data().createdTime ?? 0,
+          userId: doc.data().userId,
         });
       });
       setTodoItemList(firestoreTodoItemList);
@@ -174,6 +175,7 @@ function App() {
       todoItemContent: newTodoItem,
       isFinished: false,
       createdTime: Math.floor(Date.now() / 1000),
+      userId: currentUser,
     });
     syncTodoItemListStateWithFirestore();
   };
